1f504e04adef468cad6b133467708c5a060538db
Nicolas Debras <nicodebras17@gmail.com>
2022-05-27 13:51:56 +0200

    add heart

--- Pokemon_angular/src/app/pokemon/pokemon.component.ts
+++ Pokemon_angular/src/app/pokemon/pokemon.component.ts
@@ -16,16 +16,18 @@ export class PokemonComponent implements OnInit {
   constructor(private service:PokemonAPIService) { }
 
   nom_pokemon = "";
-  url_pokemon =""
+  url_pokemon ="";
+  pokemon_pv = "";
 
   ngOnInit(): void {
     this.service.getPokemonData().subscribe(response=>{
       //console.log(response);
-      this.nom_pokemon = response.name + response.stats[0].base_stat;
+      this.nom_pokemon = response.name;
       if (this.back == false)
         this.url_pokemon = response.sprites.front_default;
       else
         this.url_pokemon = response.sprites.back_default;
+      this.pokemon_pv = "/" + response.stats[0].base_stat
       //this.nom_pokemon = JSON.parse(response);
 
     })
